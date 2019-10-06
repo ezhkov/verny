f2249a32bb9b0faba9a2e31e8cbfd7b2698c4104
Denis Ezhkov <Dan.SKIff@gmail.com>
2019-10-06 16:05:22 +0400

    Страница стилей

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 import Header from './header';
 
 Header.initEvents();
+
+const autosuggestInput = document.querySelector('.js-autosuggest-input');
+const autosuggestList = document.querySelector('.js-autosuggest-list');
+autosuggestInput.addEventListener('focus', () => {
+  autosuggestList.classList.add('is-active');
+});
+
+autosuggestInput.addEventListener('blur', () => {
+  autosuggestList.classList.remove('is-active');
+});
