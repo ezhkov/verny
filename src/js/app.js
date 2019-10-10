31a59c2875b2f7aba231bc4910d3767cad86dca9
Denis Ezhkov <Dan.SKIff@gmail.com>
2019-10-10 19:30:03 +0400

    Опросы

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,31 @@
+import Masonry from 'masonry-layout';
 import Header from './header';
+
+let masonryList;
+
+window.addEventListener('load', () => {
+  masonryList = document.querySelector('.js-masonry');
+  if (masonryList) {
+    const msnry = new Masonry( '.js-masonry', {
+      itemSelector: '.votes-item',
+      gutter: 20,
+      columnWidth: 380
+    });
+  }
+})
 
 Header.initEvents();
 
 const autosuggestInput = document.querySelector('.js-autosuggest-input');
 const autosuggestList = document.querySelector('.js-autosuggest-list');
-autosuggestInput.addEventListener('focus', () => {
-  autosuggestList.classList.add('is-active');
-});
 
-autosuggestInput.addEventListener('blur', () => {
-  autosuggestList.classList.remove('is-active');
-});
+if (autosuggestInput) {
+  autosuggestInput.addEventListener('focus', () => {
+    autosuggestList.classList.add('is-active');
+  });
+
+  autosuggestInput.addEventListener('blur', () => {
+    autosuggestList.classList.remove('is-active');
+  });
+}
+
